8aef932a5bbd593f283d7e24b2310c918f083eed
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-10 22:25:30 +0400

    change

--- src/app/[locale]/(protected)/addPlace/page.tsx
+++ src/app/[locale]/(protected)/addPlace/page.tsx
@@ -6,7 +6,7 @@ import ImageUploaderForm from '../../../components/Forms/ImageUploaderForm';
 import PlaceStatusForm from '../../../components/Forms/PlaceStatusForm';
 import { addNewPlace } from './AddNewPlace'; // Import the server action
 import { supabase } from './../../../../lib/supabase';
-import withAuth from '../../../components/WithAuth'
+import WithAuth from '../../../components/WithAuth'
 import { z } from 'zod';
 
 // Define Zod validation schema
@@ -169,7 +169,7 @@ const AddPlace = () => {
   );
 };
 
-export default withAuth(AddPlace);
+export default WithAuth(AddPlace);
 
 
 
