68017598ac4d8e903fe6aecec63fedf69afb32f4
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-10 18:06:12 +0400

    fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,14 @@
-// postcss.config.js
 // postcss.config.mjs
 import autoprefixer from 'autoprefixer';
 import tailwindcss from 'tailwindcss';
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
+const config = {
   plugins: [
-    tailwindcss,
-    autoprefixer,
+    tailwindcss(),
+    autoprefixer(),
   ],
 };
 
-
+export default config;
 
 
