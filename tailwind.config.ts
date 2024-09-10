92029d28fedf66eead2325880db9cca71ab460a3
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-10 21:30:55 +0400

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,11 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        dark: "#312480",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
-export default config;
+}
