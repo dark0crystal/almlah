fdf124bc3ba898fe154428d7fdf5ba62423d1be5
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-10 16:38:09 +0400

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,33 @@
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import GoogleProvider from "next-auth/providers/google"
-import prisma from "./db"
-import type { Adapter } from "next-auth/adapters"
+// import { PrismaAdapter } from "@auth/prisma-adapter"
+// import GoogleProvider from "next-auth/providers/google"
+// import prisma from "./db"
+// import type { Adapter } from "next-auth/adapters"
+
+// export const authOptions = {
+//   adapter: PrismaAdapter(prisma)as Adapter,
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//     }),
+//   ],
+// }
+
+// src/lib/auth.ts
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma)as Adapter,
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-}
+  // Other NextAuth configuration options
+};
