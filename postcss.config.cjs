7a5c0e3c9ff281089c9f19be0d97390ac05545aa
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-10 19:11:39 +0400

    fix

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,4 @@
 
-module.exports={
-  plugins:{
-   tailwindcss :{},
-   autoprefixer:{},
-
-  }
-}
+module.exports = { plugins: [ 'postcss-import', 'tailwindcss/nesting', 'tailwindcss', 'autoprefixer', ] }
 
 
