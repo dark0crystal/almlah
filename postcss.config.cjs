39cc60e979caba573bd525aaa1e2c4002f53bd4f
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-10 18:50:52 +0400

    ss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,10 @@
 
 module.exports={
-  plugins:[
-    // require('postcss-import'),
-    require('tailwindcss/nesting'),
-    require('tailwindcss'),
-    require('autoprefixer'),
+  plugins:{
+   tailwindcss :{},
+   autoprefixer:{},
 
-  ]
+  }
 }
 
 
