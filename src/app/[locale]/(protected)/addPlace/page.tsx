f2f77d02be335a1533237fc86af280a325c980bc
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-10 22:43:06 +0400

    ddd

--- src/app/[locale]/(protected)/addPlace/page.tsx
+++ src/app/[locale]/(protected)/addPlace/page.tsx
@@ -7,6 +7,8 @@ import PlaceStatusForm from '../../../components/Forms/PlaceStatusForm';
 import { addNewPlace } from './AddNewPlace'; // Import the server action
 import { supabase } from './../../../../lib/supabase';
 import WithAuth from "../../../components/WithAuth"
+
+
 import { z } from 'zod';
 
 // Define Zod validation schema
