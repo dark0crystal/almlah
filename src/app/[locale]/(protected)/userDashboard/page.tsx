ff70fc69193eaa2967eb2d7ab6187e0b7c9c23fe
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-10 22:21:30 +0400

    last but not least

--- src/app/[locale]/(protected)/userDashboard/page.tsx
+++ src/app/[locale]/(protected)/userDashboard/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from 'src/lib/auth';
+import { authOptions } from '../../../../lib/auth';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { prisma } from '../../../../lib/prisma';
