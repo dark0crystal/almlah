2921d1d1b344cb7606e4b0fdb133df47f3b407d4
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-11 19:46:28 +0400

    prisma fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,25 +1,10 @@
-// import { PrismaAdapter } from '@next-auth/prisma-adapter';
-// import GoogleProvider from "next-auth/providers/google"
-// import prisma from "./db"
-// import type { Adapter } from "next-auth/adapters"
-
-// export const authOptions = {
-//   adapter: PrismaAdapter(prisma)as Adapter,
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//   ],
-// }
-
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import GoogleProvider from 'next-auth/providers/google';
-import {prisma} from './prisma';
-import { NextAuthOptions } from 'next-auth';
+import GoogleProvider from "next-auth/providers/google"
+import prisma from "./db"
+import type { Adapter } from "next-auth/adapters"
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+export const authOptions = {
+  adapter: PrismaAdapter(prisma)as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -27,3 +12,18 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 }
+
+// import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// import GoogleProvider from 'next-auth/providers/google';
+// import {prisma} from './prisma';
+// import { NextAuthOptions } from 'next-auth';
+
+// export const authOptions: NextAuthOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//     }),
+//   ],
+// }
