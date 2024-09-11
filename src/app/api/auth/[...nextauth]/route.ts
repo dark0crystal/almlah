ebdc14e209a93a2de36846c68ca9428ce06fd311
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-11 22:05:42 +0400

    may be work , auth problem

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,12 +6,19 @@
 
 
 
+// import {authOptions}from "../../../../lib/auth"
+// import NextAuth from "next-auth"
+
+// const handler = NextAuth(authOptions)
+
+// export { handler as GET, handler as POST }
 import {authOptions}from "../../../../lib/auth"
 import NextAuth from "next-auth"
 
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
+
 
 
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,73 +1,87 @@
-// import { PrismaAdapter } from '@next-auth/prisma-adapter';
-// import GoogleProvider from "next-auth/providers/google"
-// import prisma from "./db"
-// // import type { Adapter } from "next-auth/adapters"
-// import { type AuthOptions } from 'next-auth';
+// // import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// // import GoogleProvider from "next-auth/providers/google"
+// // import prisma from "./db"
+// // // import type { Adapter } from "next-auth/adapters"
+// // import { type AuthOptions } from 'next-auth';
 
-// export const authOptions : AuthOptions  = {
-//   adapter: PrismaAdapter(prisma),
-//   // as Adapter
+// // export const authOptions : AuthOptions  = {
+// //   adapter: PrismaAdapter(prisma),
+// //   // as Adapter
+// //   providers: [
+// //     GoogleProvider({
+// //       clientId: process.env.GOOGLE_CLIENT_ID as string,
+// //       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+// //     }),
+// //   ],
+// // }
+
+// // import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// // import GoogleProvider from 'next-auth/providers/google';
+// // import {prisma} from './prisma';
+// // import { NextAuthOptions } from 'next-auth';
+
+// // export const authOptions: NextAuthOptions = {
+// //   adapter: PrismaAdapter(prisma),
+// //   providers: [
+// //     GoogleProvider({
+// //       clientId: process.env.GOOGLE_CLIENT_ID!,
+// //       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+// //     }),
+// //   ],
+// // }
+
+// import { prisma } from "./prisma";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import GoogleProvider from "next-auth/providers/google";
+// import type { NextAuthOptions } from "next-auth";
+
+// export const authOptions: NextAuthOptions = {
+//   // This is a temporary fix for prisma client.
+//   // @see https://github.com/prisma/prisma/issues/16117
+//   adapter: PrismaAdapter(prisma as any),
+//   pages: {
+//     signIn: "/login", // Custom sign-in page if you have one
+//   },
 //   providers: [
 //     GoogleProvider({
 //       clientId: process.env.GOOGLE_CLIENT_ID as string,
 //       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 //     }),
 //   ],
-// }
+//   callbacks: {
+//     session: ({ session, token }) => {
+//       return {
+//         ...session,
+//         user: {
+//           ...session.user,
+//           id: token.id,
+//         },
+//       };
+//     },
+//     jwt: ({ token, user }) => {
+//       if (user) {
+//         const u = user as unknown as any;
+//         return {
+//           ...token,
+//           id: u.id,
+//         };
+//       }
+//       return token;
+//     },
+//   },
+//   secret: process.env.NEXTAUTH_SECRET, // Ensure you have this set
+// };
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import GoogleProvider from "next-auth/providers/google"
+import prisma from "./db"
+import type { Adapter } from "next-auth/adapters"
 
-// import { PrismaAdapter } from '@next-auth/prisma-adapter';
-// import GoogleProvider from 'next-auth/providers/google';
-// import {prisma} from './prisma';
-// import { NextAuthOptions } from 'next-auth';
-
-// export const authOptions: NextAuthOptions = {
-//   adapter: PrismaAdapter(prisma),
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//   ],
-// }
-
-import { prisma } from "./prisma";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import GoogleProvider from "next-auth/providers/google";
-import type { NextAuthOptions } from "next-auth";
-
-export const authOptions: NextAuthOptions = {
-  // This is a temporary fix for prisma client.
-  // @see https://github.com/prisma/prisma/issues/16117
-  adapter: PrismaAdapter(prisma as any),
-  pages: {
-    signIn: "/login", // Custom sign-in page if you have one
-  },
+export const authOptions = {
+  adapter: PrismaAdapter(prisma)as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  callbacks: {
-    session: ({ session, token }) => {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          id: token.id,
-        },
-      };
-    },
-    jwt: ({ token, user }) => {
-      if (user) {
-        const u = user as unknown as any;
-        return {
-          ...token,
-          id: u.id,
-        };
-      }
-      return token;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET, // Ensure you have this set
-};
+}
