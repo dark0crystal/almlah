e1f809b58c0568659b0f082226e8b5250aba6748
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-11 21:30:34 +0400

    add postcss

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+const createNextIntlPlugin = require('next-intl/plugin');
 
 // Create the plugin with the `createNextIntlPlugin` function
 const withNextIntl = createNextIntlPlugin();
 
 // Define the Next.js configuration
 const nextConfig = {
-
   images: {
     domains: [process.env.DOMAINS, 'lh3.googleusercontent.com'],
   },
@@ -13,7 +12,8 @@ const nextConfig = {
 };
 
 // Export the configuration with the `withNextIntl` plugin applied
-export default withNextIntl(nextConfig);
+module.exports = withNextIntl(nextConfig);
+
 
 
 
