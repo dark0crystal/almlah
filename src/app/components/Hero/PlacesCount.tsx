489a936426ce086b6b0c95421cbb0dcc8a5b6532
ALMardasDEV <albusaidi9094@gmail.com>
2024-09-22 09:04:24 +0400

    change place count

--- src/app/components/Hero/PlacesCount.tsx
+++ src/app/components/Hero/PlacesCount.tsx
@@ -3,7 +3,7 @@ import { getTranslations } from "next-intl/server"
 
 export default async function PlacesCount(){
     const t= await getTranslations('HomePage')
-    const numOfPlaces= 15
+    const numOfPlaces= 30
     return(
         <div className="text-xl font-semibold border rounded-full p-4 border-slate-800">
             <h1>{`${t("numOfPlaces")} ${numOfPlaces}  ${t('contPlaces')}`} 🎉</h1>
